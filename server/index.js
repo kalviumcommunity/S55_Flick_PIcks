ff9e94aac63ad06452bbc76acff6bec69a66864a
shaaaaz <jiwanishaaz@gmail.com>
2024-06-28 23:23:11 +0530

    render

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const { connectDB , disconnectDB , isConnected} = require('./db.js')
 const app = express()
 const Router = require('./routes.js')
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const cors = require('cors')
 
 app.options('*', (req, res) => {
@@ -13,6 +13,10 @@ app.options('*', (req, res) => {
   })     
 
 app.use(cors())
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
 
 app.use(cors({
     origin: ['https://studioo-h3vda8igl-shaazs-projects-888212a7.vercel.app/','http://localhost:5173'],
