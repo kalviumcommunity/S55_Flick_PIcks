0562df0bb8323b56b697989cd7c4f9598d93d293
shaaaaz <jiwanishaaz@gmail.com>
2024-06-28 20:11:35 +0530

    backend link

--- server/index.js
+++ server/index.js
@@ -5,6 +5,15 @@ const Router = require('./routes.js')
 const PORT = 3000
 const cors = require('cors')
 
+app.options('*', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "https://studio-frontend-ampy6loh9-shaazs-projects-888212a7.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.sendStatus(200)
+  })     
+
+app.use(cors())
+
 app.use(cors({
     origin: ['https://studioo-h3vda8igl-shaazs-projects-888212a7.vercel.app/','http://localhost:5173'],
     methods: 'GET, POST, PUT, DELETE',
